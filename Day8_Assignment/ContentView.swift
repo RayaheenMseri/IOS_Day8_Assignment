29f953321d90154c907c0a542581d261c64f1166
RayaheenMseri <rayaheenmseri@gmail.com>
2025-03-11 12:05:38 +0300

    Initial Commit

---
+++ Day8_Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day8_Assignment
+//
+//  Created by Taibah Valley Academy on 11/09/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day8_Assignment/Day8_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day8_AssignmentApp.swift
+//  Day8_Assignment
+//
+//  Created by Taibah Valley Academy on 11/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Day8_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
